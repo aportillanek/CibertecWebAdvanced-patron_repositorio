07177991b00c235fa8361635cf7e46328e333190
cibertecweb <anthony-nek22@hotmail.com>
2017-06-22 22:24:05 -0500

    Se agrego una solucion selenium

---
+++ Cibertec.2EE/Cibertec.Automation/SimpleTest.cs
@@ -0,0 +1,20 @@
+﻿using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cibertec.Automation
+{
+    public class SimpleTest
+    {
+        public static void Navigate()
+        {
+
+            var drive = new ChromeDriver();
+            drive.Navigate().GoToUrl("http://localhost:5000");
+           // drive.Close();
+        }
+    }
+}
